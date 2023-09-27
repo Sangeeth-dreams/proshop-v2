ce764b1b1240acccd96d011ee1a2cad8e6275953
SangeethIT19958170 <129158915+SangeethIT19958170@users.noreply.github.com>
2023-09-27 12:29:31 +0530

    product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,8 @@
 import express from "express";
 const router = express.Router();
-//asyncHandler middleware is used to wrap asynchronous functions and handle errors.
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import { getProducts, getProductById } from "../controllers/productController.js";
 
-//route to get all '/' products
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-}));
-
-//route to get a single product
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if(product) {
-        return res.json(product);
-    }else {
-        res.status(404);
-        throw new Error('Resource not found');
-    }
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
