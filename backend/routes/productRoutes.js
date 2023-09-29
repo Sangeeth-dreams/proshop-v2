b73bf7335c13da7253b04f91da1ab5009c6560f6
SangeethIT19958170 <129158915+SangeethIT19958170@users.noreply.github.com>
2023-09-29 11:51:02 +0530

    Product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 import { getProducts, getProductById } from "../controllers/productController.js";
 
+//route to get all products
 router.route('/').get(getProducts);
+//route to get a single product
 router.route('/:id').get(getProductById);
 
 export default router;
